d7726165f5a9e38007b23c3837ff3744618f65b2
Andres Bonilla <abonilla@eventbrite.com>
2021-04-15 11:59:39 -0300

    Create requirements files if not found

--- pfreeze/src/pfreeze.py
+++ pfreeze/src/pfreeze.py
@@ -14,20 +14,25 @@ def get_installed():
 
 def run():
     packages = []
+    try:
+        req = open('requirements.txt', 'r')
+    except FileNotFoundError:
+        req = open('requirements.txt', 'w+')
+    try:
+        req_dev = open('requirements-dev.txt', 'r')
+    except FileNotFoundError:
+        req_dev = open('requirements-dev.txt', 'w+')
+
     if len(sys.argv) == 1:
-        with open('requirements.txt') as req:
-            [print(line, end='') for line in req.readlines()]
+        [print(line, end='') for line in req.readlines()]
     elif sys.argv[1] == 'dev':
-        with open('requirements.txt') as req, open('requirements-dev.txt') as req_dev:
-            [print(line, end='') for line in req.readlines()]
-            [print(line, end='') for line in req_dev.readlines()]
+        [print(line, end='') for line in req.readlines()]
+        [print(line, end='') for line in req_dev.readlines()]
     elif sys.argv[1] == 'dev-only':
-        with open('requirements-dev.txt') as req_dev:
-            [print(line, end='') for line in req_dev.readlines()]
+        [print(line, end='') for line in req_dev.readlines()]
     elif sys.argv[1] == 'new':
-        with open('requirements.txt') as req, open('requirements-dev.txt') as req_dev:
-            pkgs = list(req.readlines())
-            pkgs.extend(req_dev.readlines())
+        pkgs = list(req.readlines())
+        pkgs.extend(req_dev.readlines())
         pkgs_new = list(get_installed())
         for item in pkgs:
             try:
@@ -38,8 +43,10 @@ def run():
     elif sys.argv[1] == 'installed':
         [print(line) for line in get_installed()]
     elif sys.argv[1] == 'all':
-        with open('requirements.txt') as req, open('requirements-dev.txt') as req_dev:
-            pkgs = set(req.readlines())
-            pkgs.update(req.readlines())
+        pkgs = set(req.readlines())
+        pkgs.update(req.readlines())
         pkgs.update(get_installed())
         [print(line) for line in pkgs]
+
+    req.close()
+    req_dev.close()
